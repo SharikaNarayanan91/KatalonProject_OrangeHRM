074d4667b74226419c0c3ae18603f41b76db6957
Sharika <Sharika@10.15.58.172>
2024-04-23 16:08:42 +0530

    Added 2 test cases

--- Test Suites/RegrssionSuiteCollection.ts
+++ Test Suites/RegrssionSuiteCollection.ts
@@ -14,7 +14,7 @@
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/LoginSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
